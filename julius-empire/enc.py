ea2e539b7201e550097c0083767367c6b7f3b5b4
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-08-08 16:24:51 -0400

    crypto/julius-empire

---
+++ julius-empire/enc.py
@@ -0,0 +1,13 @@
+flag = 'camp{caesar_what_a_cool_cipher}'
+
+enc = ''
+rot = 13
+for char in flag:
+    print(char,end='.')
+    if char in 'abcdefghijklmnopqrstuvwxyz':
+        enc += chr((ord(char) + rot-97) % 26 + 97)
+    else:
+        enc += char
+
+with open('flags.txt','a+') as f:
+    f.write(enc + '\n')
