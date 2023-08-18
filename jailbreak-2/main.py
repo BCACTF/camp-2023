4fb32bb4bd44c8d00186759da4fa89df8297ba6d
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-08-18 12:25:54 -0400

    banned char change

---
+++ jailbreak-2/main.py
@@ -0,0 +1,18 @@
+def sanitize(letter):
+    print("Checking for contraband...")
+    return any([i in letter.lower() for i in BANNED_CHARS])
+
+BANNED_CHARS = "gdvxcfiyunrdmn'~`@#$%^&*-/.{}0123456789"
+flag = "REDACTED"
+
+print("Welcome to the prison's mail center")
+msg = input("Please enter your message: ")
+
+if sanitize(msg): 
+    print("Contraband letters found!\nMessage Deleted!")
+    exit()
+
+try:
+    exec(msg)
+except Exception as e:
+    print(f'Error occured: {str(e)}; Message could not be sent, please report to system administrator')
