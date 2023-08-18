2461a426c8dc1386c887473b1faca7c5f8426bc6
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-08-18 13:01:04 -0400

    init

--- jailbreak-2/main.py
+++ jailbreak-2/main.py
@@ -3,16 +3,21 @@ def sanitize(letter):
     return any([i in letter.lower() for i in BANNED_CHARS])
 
 BANNED_CHARS = "gdvxcfiyunrdmn'~`@#$%^&*-/.{}0123456789"
+solve = "locals()[chr(102)+chr(108)+chr(97)+chr(103)]"
 flag = "REDACTED"
 
 print("Welcome to the prison's mail center")
-msg = input("Please enter your message: ")
 
-if sanitize(msg): 
-    print("Contraband letters found!\nMessage Deleted!")
-    exit()
+msg = input("\nPlease enter your message: ")
 
-try:
-    exec(msg)
-except Exception as e:
-    print(f'Error occured: {str(e)}; Message could not be sent, please report to system administrator')
+while msg != "":
+    if sanitize(msg): 
+        print("Contraband letters found!\nMessages Deleted!")
+        exit()
+
+    try:
+        exec(msg)
+    except Exception as e:
+        print(f'Error occured: {str(e)}; Message could not be sent.')
+
+    msg = input("\nPlease enter your message: ")
---
+++ jailbreak-2/solve.md
@@ -0,0 +1,2 @@
+a=True;b=a+a;e=b+a;h=e+a;j=h+a;l=(j+j)**b;
+locals()[chr(l+b)+chr(l+h+h)+chr(l-e)+chr(l+e)]
