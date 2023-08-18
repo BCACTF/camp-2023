386c1c83653f3228fea359ffdcbee2eb662e9a86
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-08-18 12:10:41 -0400

    finish

--- jailbreak/deploy.py
+++ jailbreak/deploy.py
@@ -3,7 +3,7 @@ def sanitize(letter):
     return any([i in letter.lower() for i in BANNED_CHARS])
 
 BANNED_CHARS = "gdvxftundmn'~`@#$%^&*-/.{}"
-flag = "REDACTED"
+flag = "camp{PYth0n_M4steR_M2!f45}"
 
 print("Welcome to the prison's mail center")
 msg = input("Please enter your message: ")
@@ -12,7 +12,4 @@ if sanitize(msg):
     print("Contraband letters found!\nMessage Deleted!")
     exit()
 
-try:
-    exec(msg)
-except Exception as e:
-    print(f'Error occured: {str(e)}; Message could not be sent, please report to system administrator')
+exec(msg)
