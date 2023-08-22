4c0090852e527e5c74f525114fff9748bd3c4251
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-08-22 12:52:08 -0400

    init

---
+++ java-rev-1/RevOne.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RevOne {
+   public static void main(String[] args) {
+      System.out.println("Enter the password: ");
+      Scanner scanner = new Scanner(System.in);
+
+      String line = scanner.nextLine();
+
+      if (checkPassword(line)) {
+         System.out.println("That's the right password!");
+      } else {
+         System.out.println("That's the incorrect password!");
+      }
+
+      scanner.close();
+   }
+
+   public static boolean checkPassword(String passwordGuess) {
+      String[] flagSegments = {"C0nGr4TulAt1OnS", "Y0u", "J4v4", "Pr0"};
+
+      String flag = "camp{"+String.join("_", flagSegments)+"}";
+
+      return flag.equals(passwordGuess);
+   }
+}
