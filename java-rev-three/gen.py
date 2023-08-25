78ade11cc4fbde709d7a6cadc2a9b52eeea84b6d
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-08-25 18:11:39 -0400

    make easier

--- java-rev-three/gen.py
+++ java-rev-three/gen.py
@@ -1,4 +1,6 @@
+
 flag = "camp{y0U_offic1ALLY_Be4t_jAVa_REV_!!}"
+encryptString = "qxwfrKpXJeAChMnLCHbERWO2zc701ZPxURtFi"
 
 guess = ""
 
@@ -9,9 +11,7 @@ nums = []
 avgs = []
 
 for i in range(length):
-    num1, num2 = ord(flag[i])+128, ord(flag[length-i-1])
+    num1, num2 = ord(flag[i]), ord(encryptString[i])
     nums.append(num1-num2)
-    avgs.append((num1-128+num2)/2)
 
 print(nums)
-print(avgs)
