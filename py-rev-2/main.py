8ed73949ac3bae60119613a8f79c3d5e01fa0eff
Jack Crowley <98288502+Jack-Crowley@users.noreply.github.com>
2023-08-26 17:38:46 -0400

    rev/py-rev-2 (#24)
    * code
    
    * chall
    
    * some fixes / tweaks
    
    ---------
    
    Co-authored-by: mud-ali <96320211+mud-ali@users.noreply.github.com>

---
+++ py-rev-2/main.py
@@ -0,0 +1,20 @@
+def passwordChecker(guess):
+    flag = {'D': 13, 'a': 1, 'O': 6, 'u': 7, 'r': 17, 'Y': 5, 'U': 11, '!': 27, 'c': 0, 'A': 15, 'n': 12, 'P': 23, 't': 25, '}': 28, 'h': 20, 'm': 2, 'G': 19, 'f': 9, 'p': 3, '{': 4, 'I': 18, 'T': 21, 'H': 26, '4': 24, 'o': 10}
+    
+    for i in range(len(guess)):
+        letter = guess[i]
+        if flag.get(letter) != None:
+            if flag[letter] == i:
+                continue
+        if letter == "_": continue
+        return False
+
+    return True
+
+
+guess = input("Enter the password: ")
+
+if passwordChecker(guess):
+    print("That's the right password!")
+else:
+    print("That's the incorrect password!")
