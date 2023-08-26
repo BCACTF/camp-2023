294bde5eba0e5d9cf87645e2712f628b89c4df79
mud-ali <96320211+mud-ali@users.noreply.github.com>
2023-08-26 17:59:48 -0400

    add comments to make code clearer

--- XylophOneR/given.py
+++ XylophOneR/given.py
@@ -12,11 +12,14 @@ inp = hexify(input("Tell me something: "))
 outBits = ""
 
 for index in range(0, len(inp), 2):
+    # if input is longer than flag, wrap around
     flagIndex = index%len(flag)
 
+    # get bits out of the flag and the input
     flagBits = format(int(flag[flagIndex:flagIndex+2], 16), '08b')
     inputBits = format(int(inp[index:index+2], 16), '08b')
 
+    # iterate over each bit in the byte
     for bitIndex in range(8):
         result = int(flagBits[bitIndex]) ^ int(inputBits[bitIndex])
         outBits += str(result)
@@ -25,7 +28,7 @@ out = ""
 
 for i in range(0, len(outBits), 8):
     value = int(outBits[i:i+8], 2)
-    print(value)
+    # print(value)
     out+=f"{value:02x}"
 
 print(out)
